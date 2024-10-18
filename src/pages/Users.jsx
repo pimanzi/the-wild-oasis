3e7d24ff0de2b7855320eb9e5c42fa40e5aade7f
pimanzi <p.imanzi@alustudent.com>
2024-10-18 18:11:43 +0200

    dashboard layout

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -4,7 +4,8 @@ import Heading from '../ui/Heading';
 function NewUsers() {
   return (
     <>
-      <Heading as="h1">Create a new user</Heading>;<SignupForm></SignupForm>
+      <Heading as="h1">Create a new user</Heading>
+      <SignupForm></SignupForm>
     </>
   );
 }
